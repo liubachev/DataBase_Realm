7ce481f4a1f5c4580edd031b9ad13d7266ce7f6b
liubachev <liubachev.m@gmail.com>
2020-02-19 16:16:34 +0300

    Initial Commit

---
+++ DataBase_Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataBase_Realm
+//
+//  Created by Михаил Любачев on 19.02.2020.
+//  Copyright © 2020 Михаил Любачев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
